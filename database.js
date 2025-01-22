2001a5878ebffd1ef0f1e8e98c97651e66a5bf30
Greg Korte <gregmkorte@gmail.com>
2025-01-22 10:07:34 -0600

    Expanding dino data. Adding getter for dino data.

--- database.js
+++ database.js
@@ -14,5 +14,28 @@ const database = {
       diet: "Carnivore",
       favoriteThing: "Opening doors",
     },
+    {
+      name: "Triceratops",
+      height: "10 feet",
+      weight: "12 tons",
+      diet: "Herbivore",
+      favoriteThing: "Charging with its horns",
+    },
+    {
+      name: "Ankylosaurus",
+      height: "5 feet",
+      weight: "4 tons",
+      diet: "Herbivore",
+      aFewOfMyFavoriteThings: "Swinging its clubbed tail",
+    },
+    {
+      name: "Spinosaurus",
+      height: "18 feet",
+      weight: "8 tons",
+      diet: "Carnivore",
+      favoriteThing: "Swimming and fishing",
+    },
   ],
-}
+};
+
+export const getDinosaurs = () => structuredClone(database.dinosaurs);
